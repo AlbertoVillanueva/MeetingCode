c5bdbb4ff9cee419643231ec8260e057fc8505b7
Javier <jcorrochano98@gmail.com>
2019-04-01 12:39:34 +0200

    Listas de proyectos presentables y pagina de proyecto creada

---
+++ MeetingCode/src/models/proyecto.model.ts
@@ -0,0 +1,10 @@
+export class Proyecto {
+    public key?: string; //Esto maneja el id de contacto al usar firebase
+    public nombre: string;
+    public foto: string;
+    public descripcion: string;
+
+    constructor() {
+        this.key="";
+    }
+}
--- MeetingCode/src/pages/home/home.ts
+++ MeetingCode/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Proyecto } from '../../models/proyecto.model';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,18 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  private texto = "Ahora, un equipo de investigadores de diversas instituciones, entre ellas las universidades de Nueva York y Ottawa (Canadá), descubrió que la exposición al ozono durante largo plazo tiene consecuencias negativas para la salud del ser humano.";
 
+
+  proyectosGames: Proyecto []= [{"nombre":"Pokémon Yellow","foto":"../../assets/imgs/prueba.jpg","descripcion":this.texto},
+  {"nombre":"Pokémon Red","foto":"../../assets/imgs/prueba.jpg","descripcion":this.texto},
+  {"nombre":"Pokémon Blue","foto":"../../assets/imgs/prueba.jpg","descripcion":this.texto}];
+
+  proyectosML: Proyecto []= [{"nombre":"Pokémon Yellow","foto":"../../assets/imgs/prueba.jpg","descripcion":this.texto},
+  {"nombre":"Pokémon Red","foto":"../../assets/imgs/prueba.jpg","descripcion":this.texto},
+  {"nombre":"Pokémon Blue","foto":"../../assets/imgs/prueba.jpg","descripcion":this.texto}];
+
+  irProyecto() {
+    this.navCtrl.push("ProyectoPage");
+  }
 }
